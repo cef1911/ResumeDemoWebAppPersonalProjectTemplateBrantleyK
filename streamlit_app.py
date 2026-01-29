8308788285b3cce3c13352222422ba0747b17bdc
Christopher Enam Franklin <cef1911@hotmail.com>
2026-01-29 12:29:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,12 @@
-import pandas as pd
 import streamlit as st
-import numpy as np
 
-#import plotly as px
-#import plotly.figure_factory as ff
 
 """
 # Welcome to Chris Franklin's StreamLit Web App for Deploying DataScience Projects!
 This is inspired from the StreamLit.io site.
 See the website for more info https://streamlit.io/
 
-Click the link to view Brantley Kimpson Resume
+Click the link to view a Sample Resume Template
 
 https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:800bf297-852f-45a1-bf76-4680b6831e09
 
@@ -21,12 +17,3 @@ https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:800bf297-852f-45a1-bf76-4680b6831
 #st.balloons()
 
 #st.snow()
-
-#import streamlit as st, random, time    
-
-# time.sleep(1)
-# coins = ['http://re-bol.com/heads.jpg', 'http://re-bol.com/tails.jpg']
-# coin = random.choice(coins)
-# st.image(coin)
-# if st.button('Flip'):
-#   st.experimental_rerun()
