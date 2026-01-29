fe446b785c1a481d4e7b53eae6d5c69acac34ab0
Christopher Enam Franklin <cef1911@hotmail.com>
2026-01-29 12:30:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 This is inspired from the StreamLit.io site.
 See the website for more info https://streamlit.io/
 
-Click the link to view a Sample Resume Template
+Click the link to view Brantley Kimpson's Resume
 
 https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:800bf297-852f-45a1-bf76-4680b6831e09
 
